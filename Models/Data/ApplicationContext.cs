ba1440db13ebb40c1aec60363a635944cef3c724
Alex asd <AlexeiVacari95@outlook.com>
2021-10-30 01:01:01 +0300

    Add new Model_entity and Conect_To_SQLServer

---
+++ Models/Data/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Test_App.Models.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Actor_Movie> Actor_Movies { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new { am.ActorId, am.MovieId });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(am => am.Actor).WithMany(act => act.Actor_Movies).HasForeignKey(am => am.ActorId);
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(am => am.Movie).WithMany(act => act.Actor_Movies).HasForeignKey(am => am.MovieId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
